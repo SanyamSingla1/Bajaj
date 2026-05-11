9facb4be73f34df7375843f85555e053cfc0b922
SanyamSingla1 <sanyam2705@gmail.com>
2026-05-11 20:41:18 +0530

    commit

--- maxSubArray.java
+++ maxSubArray.java
@@ -1,17 +1,12 @@
-public class maxSubArray {
-    public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        int maxSum = Integer.MIN_VALUE;
-        int currentSum = 0;
-        for (int num : nums) {
-            currentSum += num;
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
+import java.util.*;
+class maxSubArray {
+    public int maxSubArray(int[] nums) {
+        int curr = 0;
+        int maxsum = nums[0];
+        for(int x : nums){
+            curr = Math.max(x,curr+x);
+            maxsum = Math.max(maxsum,curr);
         }
-        System.out.println("Maximum subarray sum: " + maxSum);
+        return maxsum;
     }
 }
